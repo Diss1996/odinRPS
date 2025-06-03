2105a15437b19b8cd30924536afc4cca0953cda0
Diss1996 <209380604+Diss1996@users.noreply.github.com>
2025-06-03 16:16:32 -0300

    added buttons

--- scripts.js
+++ scripts.js
@@ -15,21 +15,6 @@ function getComputerChoice(){
     return result;
 }
 
-function getHumanChoice(choice){
-
-    choice = choice.toLowerCase();
-    if (choice != "rock" && choice != "scissors" && choice != "paper"){
-        choice = null;
-        console.log("Human choice was invalid");
-        return null;
-    }
-
-    result = choice.charAt(0).toUpperCase() + choice.slice(1);
-
-    console.log("Human chooses: "+result);
-    return result;
-}
-
 function playRound(computerChoice, humanChoice){
     let result = null;
 
@@ -51,35 +36,98 @@ function playRound(computerChoice, humanChoice){
         result = "Invalid! Something went wrong"
     }
 
-    console.log("Results for this round: "+result);
+    
     return result;
 }
 
 function playGame(){
  let humanScore = 0;
  let computerScore = 0;
- for(i = 0; i < 5; i++){    
-        console.log("Round "+(i + 1) )
-        let humanChoice = getHumanChoice(window.prompt("Rock, Paper, or Scissors?"));
-    if (humanChoice !== null) {
-        let computerChoice = getComputerChoice();
-        result = playRound(computerChoice, humanChoice);
-        if(result == "Human Wins"){
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {   
+    humanChoice = button.textContent;
+    playRound()
+    console.log("Human chose "+humanChoice)
+    let computerChoice = getComputerChoice();
+    result = playRound(computerChoice, humanChoice);
+     if(result == "Human Wins"){
             humanScore++;
         }  else if (result == "Computer Wins") {
             computerScore++;
         } else if (result == "Tie"){
-            console.log("Tie! No points awarded")
+            result += "! No points awarded"
         } else {
-            console.log("Something went wrong")
+            winner.textContent = "Something went wrong";
         }
-    } else {
-        console.log("Skipping round due to invalid input.");
-    }
+        console.log("Current Scores; Human: "+humanScore+" Computer: "+computerScore);
+        hChoice.textContent = "Human chooses "+humanChoice;
+        cChoice.textContent = "Computer chooses: "+computerChoice;
+        winner.textContent = result+ ": Current Scores; Human: "+humanScore+" Computer: "+computerScore;
+        
+        if(humanScore >= 5 || computerScore >= 5){
+            winner.textContent = 
+            humanScore > computerScore
+            ? "Human wins! Final Score: Human " + humanScore + ", Computer " + computerScore
+            : humanScore < computerScore
+                ? "Computer wins! Final Score: Human " + humanScore + ", Computer " + computerScore
+                : "It's a tie! Final Score: Human " + humanScore + ", Computer " + computerScore;
+
+            humanScore = 0;
+            computerScore = 0;
+         }
+        
+  });
+});
 }
 
-console.log("Final Scores; Human: "+humanScore+" Computer: "+computerScore);
-}
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+rock.textContent = "Rock";
+rock.classList.add("btn");
+rock.id = "rockBtn";
+
+paper.textContent = "Paper";
+paper.classList.add("btn");
+paper.id = "paperBtn";
+
+scissors.textContent = "Scissors";
+scissors.classList.add("btn");
+scissors.id = "scissorsBtn";
+
+const results = document.createElement("div");
+const hChoice = document.createElement("p");
+const cChoice = document.createElement("p");
+const winner = document.createElement("p");
+
+
+document.body.appendChild(rock);
+document.body.appendChild(paper);
+document.body.appendChild(scissors);
+document.body.appendChild(results);
+
+results.appendChild(hChoice);
+results.appendChild(cChoice);
+results.appendChild(winner);
+
+const buttons = document.querySelectorAll(".btn");
 
 
 playGame();
+
+// function getHumanChoice(choice){
+
+//     choice = choice.toLowerCase();
+//     if (choice != "rock" && choice != "scissors" && choice != "paper"){
+//         choice = null;
+//         console.log("Human choice was invalid");
+//         return null;
+//     }
+
+//     result = choice.charAt(0).toUpperCase() + choice.slice(1);
+
+//     console.log("Human chooses: "+result);
+//     return result;
+// }
